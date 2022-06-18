7e648022d660dc9d1ea890a8ae52686380b9e47c
Daniil <warcatyt>
2022-06-19 02:21:19 +0300

    Исправлено название модели

--- ScheduleSystem/models.py
+++ ScheduleSystem/models.py
@@ -23,7 +23,7 @@ class Students(models.Model):
     # @receiver(post_save, sender=User)
     # def create_or_update_user_profile(sender, instance, created, **kwargs):
     #     if created:
-    #         Profile.objects.create(user=instance)
+    #         Students.objects.create(user=instance)
     #     instance.profile.save()
 
 
