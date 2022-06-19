2bd5559aec453e9880488b629e14a6e75207f49e
Daniil <warcatyt>
2022-06-19 03:33:44 +0300

    Обновил способ вывода расписания по новым моделям: - теперь показывается расписание на сегодняшний ДЕНЬ НЕДЕЛИ

--- ScheduleSystem/templatetags/news_tags.py
+++ ScheduleSystem/templatetags/news_tags.py
@@ -1,10 +1,29 @@
+import datetime
 from django import template
-from ScheduleSystem.models import Attendance
-from datetime import date
+from ScheduleSystem.models import Schedules
+
 register = template.Library()
 
 
 @register.inclusion_tag('ScheduleSystem/list_categories.html')
 def show_categories():
-    schedule_today = Attendance.objects.filter(date_attendance=date.today()).order_by('id_lesson__number_lesson')
+    weekdaynow = datetime.datetime.today().isoweekday()
+    weekdayname = 'Неизвестно'
+    if weekdaynow == 1:
+        weekdayname = 'Понедельник'
+    elif weekdaynow == 2:
+        weekdayname = 'Вторник'
+    elif weekdaynow == 3:
+        weekdayname = 'Среда'
+    elif weekdaynow == 4:
+        weekdayname = 'Четверг'
+    elif weekdaynow == 5:
+        weekdayname = 'Пятница'
+    elif weekdaynow == 6:
+        weekdayname = 'Суббота'
+    elif weekdaynow == 7:
+        weekdayname = 'Воскресенье'
+
+    schedule_today = Schedules.objects.filter(id_week_day__name_day=weekdayname).order_by(
+        'id_lessons_time__number_lesson')
     return {"schedule_today": schedule_today}
