e8b65c5a1d6a94d2b75a4e623db054a3d34c707b
Daniil <warcatyt>
2022-06-21 01:08:42 +0300

    - обновлены картинки у предметов

--- ScheduleSystem/admin.py
+++ ScheduleSystem/admin.py
@@ -40,7 +40,7 @@ class SubjectsAdmin(admin.ModelAdmin):
     list_display = (
         'name_subject', 'id_teacher', 'id_department', 'id_course', 'id_educational_form', 'id_group', 'exam_form',
         'hours')
-    list_display_links = ('id_name_subject',)
+    list_display_links = ('name_subject',)
     list_filter = ('id_teacher__name_teacher', 'id_department__name_department', 'id_course__number_course',
                    'id_educational_form__name_educational_form', 'id_group')
 
